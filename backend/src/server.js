1e4d01240931f6b6538c795aac6fffeab8374e12
cyrus_w <cyrusw89@gmail.com>
2024-05-27 18:06:20 +1200

    initial setup

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import { app } from './app.js';
+
+const port = process.env.PORT || 3000;
+
+// Start the Express server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+}).on("error", (error) => {
+  // Handle errors that occur when starting the server
+  if (error.code === "EADDRINUSE") {
+    console.log("PORT is already in use.");
+  } else {
+    console.log("Server Errors: ", error);
+  }
+});
